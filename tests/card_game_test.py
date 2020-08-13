3bf629986c7236ec256ac8bd7001e2f428f8d724
Duncan Ryan <duncancryan1@gmail.com>
2020-08-13 21:31:47 +0100

    initiated property tests with pass keyword

--- tests/card_game_test.py
+++ tests/card_game_test.py
@@ -10,6 +10,12 @@ class TestCardGame(unittest.TestCase):
         self.card2 = Card("Clubs", 4)
         self.card_game_01 = CardGame(self.card1, self.card2)
 
+    def test_game_has_card_01(self):
+        pass
+
+    def test_game_has_card_02(self):
+        pass
+
     def test_can_check_for_ace(self):
         pass
 
